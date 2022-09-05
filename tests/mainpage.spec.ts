e677254244903e6f91e24d3a190096273cefcb83
Oleksandr Muliar <58072813+oleksandrMuliar@users.noreply.github.com>
2022-09-05 18:42:15 +0300

    cleanup (#3)

--- tests/mainpage.spec.ts
+++ tests/mainpage.spec.ts
@@ -10,7 +10,6 @@ test.skip('Main page have required components', async ({ page }) => {
   
   const mpage = new MainPage(page);    
   await mpage.open();
-  // await page.waitForFunction(() => document.title === 'QA Test');
   await expect(mpage.openReportUpdateButton).toBeVisible();
   await expect(mpage.openReportUpdateButton).toBeVisible();
   await expect(mpage.reportUpdatePanelHeader).toBeHidden();
@@ -22,7 +21,6 @@ test.skip('Open Report Update Panel works properly', async ({ page }) => {
   
   const mpage = new MainPage(page);
   await mpage.open();
-  await mpage.openReportUpdateButton.click();
   await mpage.invokeUpdatePanel();
   // check that Panel items are displayed  
   await expect(mpage.reportUpdatePanelHeader).toBeVisible();
@@ -38,9 +36,11 @@ test('Add item to report', async ({ page }) => {
   await mpage.open();
   await mpage.invokeUpdatePanel();
   await mpage.addItemToReport("Ford Cortina");
-  await mpage.isItemInReport("Ford Cortina");
+  await mpage.addItemToReport("Rolls Royce Wraith");
+  // await mpage.isItemInReport("Ford Cortina");
   // tmp to remove
-  await expect(mpage.reportUpdatePanelHeader).toBeHidden();
-  
+
+  // await mpage.isItemInReport("test1");
+  expect(mpage.isItemInReport("Ford Cortina")).toEqual(true);
 
 });
