4cb86731ee986abac122e0c6779af851bd3fcb8b
Oleksandr Muliar <oleksandr_muliar@epam.com>
2022-09-06 12:31:06 +0300

    WIP

--- tests/mainpage.spec.ts
+++ tests/mainpage.spec.ts
@@ -29,7 +29,7 @@ test.skip('Open Report Update Panel works properly', async ({ page }) => {
   await expect(mpage.closeReportUpdateButton).toBeVisible();
 
 });
-
+// to finish
 test.skip('Open & Close Report Update Panel can be closed', async ({ page }) => {
   
   const mpage = new MainPage(page);
@@ -48,6 +48,7 @@ test.skip('Open & Close Report Update Panel can be closed', async ({ page }) => 
   await expect(mpage.closeReportUpdateButton).toBeHidden();
 });
 
+// to finish
 test.skip('Add item to report', async ({ page }) => {
   
   const mpage = new MainPage(page);
@@ -64,23 +65,25 @@ test.skip('Add multiply items to report', async ({ page }) => {
   await mpage.open();
   await mpage.invokeUpdatePanel();
   await mpage.addItemToReport("Ford Cortina");
-  // await expect(mpage.isItemInReport("Ford Cortina")).toEqual(true); 
   await mpage.addItemToReport("Ferrari 812 Superfast");
   await mpage.addItemToReport("Seat Tarraco");  
 
 });
 
+// to finish
 test('Generate full update report', async ({ page }) => {
   
   const mpage = new MainPage(page);
   await mpage.open();
   await mpage.invokeUpdatePanel();
-  // check that Panel items are displayed  
   await mpage.addItemToReport("Ford Cortina");
   await mpage.addItemToReport("Ferrari 812 Superfast");
   await mpage.addItemToReport("Seat Tarraco");
   await mpage.generateReport();
 
   await expect(mpage.createReportNotification).toContainText('FINISHED');
-
+  await mpage.isItemInReport("Ford Cortina");
+  await mpage.isItemInReport("Seat Tarraco");
+  await mpage.isItemInReport("Ferrari 812 Superfast");
+  
 });
